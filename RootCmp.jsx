a1d725327d422df6a808d1bb4de3663d0594f03a
Jonathan Dolan <yonidolan1@gmail.com>
2024-06-26 11:38:37 +0300

    Add UserMsg in RootCmp

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -6,6 +6,7 @@ import { About } from "./pages/About.jsx"
 import { Home } from "./pages/Home.jsx"
 import { MailIndex } from "./apps/mail/pages/MailIndex.jsx"
 import { NoteIndex } from "./apps/note/pages/NoteIndex.jsx"
+import { UserMsg } from "./cmps/UserMsg.jsx"
 
 
 
@@ -13,12 +14,15 @@ export function App() {
     return <Router>
         <section className="app">
             <AppHeader />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/mail" element={<MailIndex />} />
-                <Route path="/note" element={<NoteIndex />} />
-            </Routes>
+            <main className="main-layout">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/mail" element={<MailIndex />} />
+                    <Route path="/note" element={<NoteIndex />} />
+                </Routes>
+            </main>
+            <UserMsg />
         </section>
     </Router>
 }
