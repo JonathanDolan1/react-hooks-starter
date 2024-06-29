48e66124c120bf350a0269af84862cbeec6c803f
nShahaf <shahafnoked@gmail.com>
2024-06-29 10:47:15 +0300

    replace home header with app header App header

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,16 +1,44 @@
-const { Link, NavLink } = ReactRouterDOM
+const { Link } = ReactRouterDOM
+const { useState } = React
 
 export function AppHeader() {
+    const [isOpen, setIsOpen] = useState(true)
 
-    return <header className="app-header">
-        <Link to="/">
-            <h3>LOGO!</h3>
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail/list">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
-    </header>
+
+    function onBarsClick() {
+        setIsOpen(prevIsOpen => !prevIsOpen)
+    }
+
+    const navClass = isOpen ? 'app-header-apps hidden' : 'app-header-apps'
+
+    return (
+        <section className="app-header-container">
+
+            <div className='app-header-logo-cotainer'>
+                <i className="fa-solid fa-circle-nodes"></i>
+                <div className="app-header-logo">Appsus</div>
+            </div>
+
+            <div className="app-header-icons">
+                <i className="fa-solid fa-arrow-rotate-right"></i>
+                <Link to="/mail" ><i className="fa-regular fa-envelope"></i></Link>
+                <i className="fa-solid fa-gear"></i>
+                <i className="fa-solid fa-bars" onClick={onBarsClick}>
+                    <div className={navClass}>
+                        <Link to="/"><i className="fa-solid fa-house home-app"></i></Link>
+                        <Link to="/mail/list" ><i className="fa-solid fa-envelope mail-app"></i></Link>
+                        <Link to="/note" ><i className="fa-solid fa-note-sticky note-app"></i></Link>
+                        <Link to="/about"><i className="fa-solid fa-address-card about-app"></i></Link>
+                        <Link to="/about"><i className="fa-solid fa-book book-app"></i></Link>
+                        <i className="fa-brands fa-google"></i>
+                        <i className="fa-brands fa-google-play"></i>
+                        <i className="fa-brands fa-google-wallet"></i>
+                        <i className="fa-brands fa-google-pay"></i>
+                    </div>
+                </i>
+                <div className="app-header-user-icon-s">S</div>
+                <div className="app-header-user-icon-y">Y</div>
+            </div>
+        </section>
+    )
 }
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,14 +1,12 @@
-import { HomeHeader } from "../cmps/HomeHeader.jsx";
 
 export function Home() {
     return (
         <section className="home-container">
-            <HomeHeader />
             <h1 className="home-header">Welcome to Appsus</h1>
             <h1 className="home-header">Your All-in-One Mail and Notes App</h1>
 
             <p className="home-text">Appsus seamlessly integrating powerful tools for managing emails and notes in one intuitive platform.</p>
-            
+
             <div className='home-img-container'>
                 <img className='iphone-demo' src="/apps/note/imgs/iphone-demo.png" alt="iphone demo"></img>
             </div>
